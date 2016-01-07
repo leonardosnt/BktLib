3df3010b00e660da3552f9e6a891f9d6576700a0
Leonardo Santos <leonardoosantos98@hotmail.com>
2016-01-06 22:10:17 -0200

    More stuffs & docs

--- src/io/github/bktlib/item/builders/ItemBuilder.java
+++ src/io/github/bktlib/item/builders/ItemBuilder.java
@@ -6,24 +6,67 @@ import org.bukkit.inventory.ItemStack;
 
 import io.github.bktlib.item.builders.impl.ItemBuilderImpl;
 
+/**
+ * 
+ * @author Leonardosc
+ */
 public interface ItemBuilder 
 {
+	
+	/**
+	 * Define o tipo do item.
+	 * 
+	 * @param mat Material desejado
+	 */
 	ItemBuilder type( Material mat );
 	
-	ItemBuilder durability( short durability );
+	/**
+	 * Define a durabilidade do item
+	 * 
+	 * @param durability Durabilidade desejada
+	 */
+	ItemBuilder durability( int durability );
 	
+	/**
+	 */
 	ItemBuilder maxDurability();
 	
+	/**
+	 * Define a quantidade do item
+	 * 
+	 * @param amount Quantidade desejada
+	 */
 	ItemBuilder amount( int amount );
 	
+	/**
+	 * Define o nome do item.
+	 * 
+	 * @param displayName Nome desejado
+	 */
 	ItemBuilder name( String displayName );
 	
+	/**
+	 * Adiciona linhas ao lore do item
+	 * 
+	 * @param lines Linhas para adicionar
+	 */
 	ItemBuilder lore( String ... lines );
 	
+	/**
+	 * Adiciona um encantamento ao item.
+	 * 
+	 * @param ench Tipo do encantamento
+	 * @param level Level do encantamento
+	 */
 	ItemBuilder enchant( Enchantment ench, int level );
 	
+	/**
+	 * @return O ItemStack "construido"
+	 */
 	ItemStack build();
 	
+	/**
+	 */
 	static ItemBuilder newBuilder()
 	{
 		return new ItemBuilderImpl();
--- src/io/github/bktlib/item/builders/impl/ItemBuilderImpl.java
+++ src/io/github/bktlib/item/builders/impl/ItemBuilderImpl.java
@@ -11,6 +11,7 @@ import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
 import io.github.bktlib.item.builders.ItemBuilder;
@@ -32,10 +33,12 @@ public class ItemBuilderImpl implements ItemBuilder
 	}
 
 	@Override
-	public ItemBuilder durability( short durability )
+	public ItemBuilder durability( int durability )
 	{
-		item.setDurability( (short) 
-				(item.getType().getMaxDurability() - durability) );
+		Preconditions.checkArgument( durability <= Short.MAX_VALUE,
+				"durability must less or equals than %s (Short.MAX_VALUE)", Short.MAX_VALUE );
+		
+		item.setDurability( (short) (item.getType().getMaxDurability() - durability) );
 		return this;
 	}
 
@@ -48,6 +51,8 @@ public class ItemBuilderImpl implements ItemBuilder
 	@Override
 	public ItemBuilder amount( int amount )
 	{
+		Preconditions.checkArgument( amount > 0, "amount must be positive");
+		
 		item.setAmount( amount );
 		return this;
 	}
@@ -55,13 +60,21 @@ public class ItemBuilderImpl implements ItemBuilder
 	@Override
 	public ItemBuilder name( String displayName )
 	{
+		Preconditions.checkNotNull( displayName, "displayName cannot be null");
+		
 		modifyMeta( meta -> meta.setDisplayName( displayName ) );
+		
 		return this;
 	}
 
 	@Override
 	public ItemBuilder lore( String... lines )
 	{
+		Preconditions.checkNotNull( lines, "lines cannot be null");
+		
+		if ( lines.length == 0 ) 
+			return this;
+		
 		modifyMeta( meta -> 
 		{
 			ArrayList<String> lore = Lists.newArrayList();
@@ -81,7 +94,10 @@ public class ItemBuilderImpl implements ItemBuilder
 	@Override
 	public ItemBuilder enchant( Enchantment ench, int level )
 	{
+		Preconditions.checkArgument( level > 0, "level must be positive");
+		
 		item.addUnsafeEnchantment( ench, level );
+		
 		return this;
 	}
 
