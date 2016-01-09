0190c672d1f8fcf7e8a6360ec7562918d8d67185
Leonardo Santos <leonardoosantos98@hotmail.com>
2016-01-09 00:29:16 -0200

    Adicionado exemplo

---
+++ examples/ItemBuilderExample.java
@@ -0,0 +1,28 @@
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import io.github.bktlib.item.builders.ItemBuilder;
+
+public class ItemBuilderExample
+{
+	public void example()
+	{
+		Player anPlayer = /* ... */ null;
+		
+		ItemStack coolSword = ItemBuilder.newBuilder()
+            				 .type( Material.STONE_SWORD )
+            				 .name( "&3Xablau's sword!" )
+            				 .lore( "&AUm lore legal" )
+            				 .lore( "§6Que da pra colocar" )
+            				 .lore( "§6Varias linhas" )
+            				 .enchant( Enchantment.DAMAGE_ALL, 10 )
+            				 .enchant( Enchantment.FIRE_ASPECT, 10 )
+            				 .amount( 24 )
+            				 .durability( 123 )
+            				 .build();
+		
+		anPlayer.getInventory().addItem( coolSword )
+	}
+}
