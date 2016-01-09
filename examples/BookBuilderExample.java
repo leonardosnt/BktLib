dbba733cc0bffb68040a9b8040bbee0554d51a4a
Leonardo Santos <leonardoosantos98@hotmail.com>
2016-01-09 21:24:31 -0200

    Adicionado BookBuilderExample.java

---
+++ examples/BookBuilderExample.java
@@ -0,0 +1,27 @@
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import io.github.bktlib.item.builders.BookBuilder;
+import io.github.bktlib.item.builders.ItemBuilder;
+
+public class ItemBuilderExample
+{
+	public void example()
+	{
+		Player anPlayer = /* ... */ null;
+		
+		ItemStack book = BookBuilder.newBuilder()
+        				.author( "&3leonardo&csc" )
+        				.title( "&3xablaus book" )
+        				.newPage()
+        				.line( "&bpagina xablaustica" )
+        				.lines( "&3com varias linhas", "§6coloridas" )
+        				.newPage()
+        				.line( "&bE varias paginas" )
+        				.build();
+		
+		anPlayer.getInventory().addItem( coolSword )
+	}
+}
